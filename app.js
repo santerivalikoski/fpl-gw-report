5e84cdf4d00ec0f5f202d2ff341941b415c0e3ee
Santeri Valikoski <santeri.valikoski@gmail.com>
2019-10-29 22:51:28 +0200

    toimii ihan jees top11

--- app.js
+++ app.js
@@ -1,67 +1,102 @@
 const puppeteer = require('puppeteer');
-let bookingUrl = 'https://fantasy.premierleague.com/entry/403555/event/10';
+const fs = require('fs');
+const gw = 10
+let bookingUrlList = [
+    `https://fantasy.premierleague.com/entry/403555/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/2491640/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/937861/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/152498/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/686663/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/2262018/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/3071221/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/3347188/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/3041619/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/4329720/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/3656504/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/85564/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/488333/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/1748058/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/424204/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/1273510/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/635637/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/3892961/event/${gw}`,
+    `https://fantasy.premierleague.com/entry/2302161/event/${gw}`
+];
+let managers = [{}]
+var pituus = bookingUrlList.length
+function tauko() {
+    console.log('tauko')
+}
+for (var i = 0; i < 19; i++) {
+    let bookingUrl = bookingUrlList[i];
+    (async () => {
+        process.setMaxListeners(20)
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        // await page.setViewport({ width: 1920, height: 926 });
+        await page.goto(bookingUrl, { waitUntil: 'networkidle2' });
+        // get player details
+        let teamData = await page.evaluate(() => {
+            let players = [];
+            let manager = document.querySelector('div[class="sc-bdVaJa jWQvkU"] > h2[class="Entry__EntryName-sc-1kf863-0 frXpNV"]').innerText
+            let captains = []
+            // get the player elements
+            let playersElms = document.querySelectorAll('div[class="Pitch__PitchUnit-sc-1mctasb-3 gYXrCB"]')
 
-let managers = []
-(async () => {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 926 });
-    await page.goto(bookingUrl);
-    // get player details
-    let teamData = await page.evaluate(() => {
-        let players = [];
-        let manager = document.querySelector('div[class="sc-bdVaJa jWQvkU"] > h2[class="Entry__EntryName-sc-1kf863-0 frXpNV"]').innerText
-        
-        // get the player elements
-        let playersElms = document.querySelectorAll('div[class="Pitch__PitchUnit-sc-1mctasb-3 gYXrCB"]')
-       
-        // get the player data
-        playersElms.forEach((playerElement) => {
-            let playerJson = {};
-            try {
-                playerJson.name = playerElement.querySelector('div[class="PitchElementData__ElementName-sc-1u4y6pr-0 hZsmkV"]').innerText
-                playerJson.captain = false
-                playerJson.manager = manager
-                if(playerElement.querySelector('svg[class="TeamPitchElement__StyledCaptain-sc-202u14-1 giBNVk"]'))   { 
-                    playerJson.captain = true
+            // get the player data
+            playersElms.forEach((playerElement) => {
+                let playerJson = {};
+                try {
+                    playerJson.name = playerElement.querySelector('div[class="PitchElementData__ElementName-sc-1u4y6pr-0 hZsmkV"]').innerText
+                    playerJson.captain = false
+                    playerJson.manager = manager
+                    if (playerElement.querySelector('svg[class="TeamPitchElement__StyledCaptain-sc-202u14-1 giBNVk"]')) {
+                        playerJson.captain = true
+                    }
+
+
                 }
-   
+                catch (exception) {
 
-            }
-            catch (exception){
+                }
+                players.push(playerJson);
+            });
+            playersElms.forEach((playerElement) => {
+                let yellowPlayer = {};
+                try {
+                    yellowPlayer.name = playerElement.querySelector('div[class="PitchElementData__ElementName-sc-1u4y6pr-0 fXSnzv"]').innerText
+                    yellowPlayer.captain = false
+                    yellowPlayer.manager = manager
+                    if (playerElement.querySelector('svg[class="TeamPitchElement__StyledCaptain-sc-202u14-1 giBNVk"]')) {
+                        yellowPlayer.captain = true
+                    }
 
-            }
-            players.push(playerJson);
+                }
+                catch (exception) {
+
+                }
+                players.push(yellowPlayer);
+            });
+            return players;
         });
-        playersElms.forEach((playerElement) => {
-            let yellowPlayer = {};
-            try {
-                yellowPlayer.name = playerElement.querySelector('div[class="PitchElementData__ElementName-sc-1u4y6pr-0 fXSnzv"]').innerText
-                yellowPlayer.captain = false
-                yellowPlayer.manager = manager
-                if(playerElement.querySelector('svg[class="TeamPitchElement__StyledCaptain-sc-202u14-1 giBNVk"]'))   { 
-                    yellowPlayer.captain = true
-                }  
-
-            }
-            catch (exception){
-
-            }
-            players.push(yellowPlayer);
-        });
-        return players;
-    });
-
-
-   
-
-    // for(var i = teamData.length; i > -1; i--) {
-    //     if (i === 0 || i === 1 || i === 3 || i === 4) {
-    //         delete teamData[i]
-    //     }
-    // }
-
-    console.dir(teamData);
-    await browser.close()
-})();  
-
+        managers.push(teamData)
+        // console.dir(teamData);
+        // fs.writeFile("./db.json", JSON.stringify(teamData), function(err) {
+        //     if (err) {
+        //         return console.log(err)
+        //     }
+        //     console.log("The file was saved!");
+        // })
+        await browser.close()
+    })();
+}
+setTimeout(() => {
+    fs.writeFile("./db.json", JSON.stringify(managers), function(err) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("The file was saved!");
+    })
+}, 150000);
+console.log('----LOPPU-----')
+console.dir(managers)
